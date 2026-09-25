fde95f1189198ea85d87290871eb2a46483df2bf
shoutjoy <shoutjoy@users.noreply.github.com>
2026-09-25 21:00:28 +0000

    deploy: 74b242eb10a081c7ecc18e764969aacd88d6e85d

--- mdpro/Apps/PWA/service-worker.js
+++ mdpro/Apps/PWA/service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX = 'md-viewer-pwa-';
-const CACHE_VERSION = '20260925-nonprinting-2';
+const CACHE_VERSION = '20260926-a4-multipage-1';
 const STATIC_CACHE = CACHE_PREFIX + 'static-' + CACHE_VERSION;
 const RUNTIME_CACHE = CACHE_PREFIX + 'runtime-' + CACHE_VERSION;
 const MAX_RUNTIME_ENTRIES = 80;
